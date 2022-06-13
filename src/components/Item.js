39d4d832f7ff07c74b47989fe5491697a7a8db54
Hassane24 <hsisane5@gmail.com>
2022-06-13 18:20:57 +0100

    incrementing quantity func working

--- src/components/Item.js
+++ src/components/Item.js
@@ -4,7 +4,7 @@ const Item = (props) => {
       <img src={require(`../assets/${props.src}.jpeg`)} alt={props.alt} />
       <div className="item-name">{props.itemName}</div>
       <div className="price">{props.price}</div>
-      <button className="add-to-cart" onClick={props.handleClick}>
+      <button className="add-to-cart" onClick={props.handleClick} id={props.id}>
         Add to cart
       </button>
     </div>
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -5,52 +5,67 @@ const Shop = () => {
   const [items, setItems] = useState([
     {
       name: "Molten-GF7-Composite-Basketball",
-      price: "",
-      quantityInCart: "",
+      price: "130$",
+      quantityInCart: 0,
       id: 0,
     },
     {
       name: "Wilson-Evolution-Indoor-Game-Basketball",
-      price: "",
-      quantityInCart: "",
+      price: "100$",
+      quantityInCart: 0,
       id: 1,
     },
-    { name: "Nike-Air-Jordan-4", price: "", quantityInCart: "", id: 2 },
+    {
+      name: "Nike-Air-Jordan-Retro-4",
+      price: "329$",
+      quantityInCart: 0,
+      id: 2,
+    },
     {
       name: "Kobe-11-Elite-Last-Emperor",
-      price: "",
-      quantityInCart: "",
+      price: "400$",
+      quantityInCart: 0,
       id: 3,
     },
-    { name: "Michael-Jordan-Jersey", price: "", quantityInCart: "", id: 4 },
-    { name: "Kobe-Bryant-Jersey", price: "", quantityInCart: "", id: 5 },
+    { name: "Michael-Jordan-Jersey", price: "30$", quantityInCart: 0, id: 4 },
+    { name: "Kobe-Bryant-Jersey", price: "80$", quantityInCart: 0, id: 5 },
     {
       name: "Mini-Pro-Xtreme-Basketball-Hoop-Set",
-      price: "",
-      quantityInCart: "",
+      price: "400$",
+      quantityInCart: 0,
       id: 6,
     },
     {
       name: "Basketball-Hoop-Set-For-Kids",
-      price: "",
-      quantityInCart: "",
+      price: "160$",
+      quantityInCart: 0,
       id: 7,
     },
   ]);
 
-  const removeDashFromItemName = (string) => {
-    string = string.split("-").join(" ");
-    return string;
+  const removeDashFromItemName = (string) => string.split("-").join(" ");
+
+  const addToCart = (e) => {
+    const btnId = parseInt(e.target.attributes.id.textContent);
+    setItems((prevState) => {
+      const newState = [...prevState];
+      newState.find((item) => item.id === btnId).quantityInCart += 1;
+      return newState;
+    });
   };
 
   return (
     <div className="shop">
+      <button onClick={() => console.log(items)}>asdasdasd</button>
       {items.map((item) => (
         <Item
           key={item.id}
           src={item.name}
           alt={item.name}
           itemName={removeDashFromItemName(item.name)}
+          handleClick={addToCart}
+          id={item.id}
+          price={item.price}
         ></Item>
       ))}
     </div>
