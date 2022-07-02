aec19bf9fd9205a55d7c595619b1ae71b424e52c
Hassane24 <hsisane5@gmail.com>
2022-07-02 18:37:20 +0100

    added functionality to cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,31 +1,34 @@
 import CartItem from "./CartItem";
 import "../styles/Cart.css";
 const Cart = (props) => {
-  return (
-    <div className="shopping-bag">
-      <button className="close-btn">&times;</button>
-      <h2 className="bag-h2">Your Bag</h2>
-      {props.itemsInCart.map((item) => (
-        <CartItem
-          key={item.id}
-          id={item.id}
-          imgSrc={item.name}
-          imgAlt={item.name}
-          type={item.type}
-          name={item.name}
-          numberItem={item.quantityInCart}
-          minusHandler={props.minusHandler}
-          price={item.price * item.quantityInCart}
-          onChangeHandler={props.itemInputHandler}
-          plusHandler={props.plusHandler}
-        ></CartItem>
-      ))}
-      <div className="total-price">Total Price: {props.totalPrice}$</div>
-      <button className="checkout" onClick={props.checkoutHandler}>
-        Checkout
-      </button>
-    </div>
-  );
+  if (!Array.isArray(props.itemsInCart) || !props.itemsInCart.length)
+    return <div>No items in the bag</div>;
+  else
+    return (
+      <div className="shopping-bag">
+        <button className="close-btn">&times;</button>
+        <h2 className="bag-h2">Your Bag</h2>
+        {props.itemsInCart.map((item) => (
+          <CartItem
+            key={item.id}
+            id={item.id}
+            imgSrc={item.name}
+            imgAlt={item.name}
+            type={item.type}
+            name={item.name}
+            numberItem={item.quantityInCart}
+            minusHandler={props.minusHandler}
+            price={item.price * item.quantityInCart}
+            onChangeHandler={props.itemInputHandler}
+            plusHandler={props.plusHandler}
+          ></CartItem>
+        ))}
+        <div className="total-price">Total Price: {props.totalPrice}$</div>
+        <button className="checkout" onClick={props.checkoutHandler}>
+          Checkout
+        </button>
+      </div>
+    );
 };
 
 export default Cart;
